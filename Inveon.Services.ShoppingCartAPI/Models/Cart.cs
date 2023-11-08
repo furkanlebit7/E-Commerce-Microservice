0988b98c905730055037c1c7d7abb58dbbaff480
furkanlebit7 <furkanlebitt7@gmail.com>
2023-11-08 20:50:21 +0300

    rabbitMQ queue added and connected, Product control panel for Admin added

---
+++ Inveon.Services.ShoppingCartAPI/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace Inveon.Services.ShoppingCartAPI.Models
+{
+    public class Cart
+    {
+        public CartHeader CartHeader { get; set; }
+        public IEnumerable<CartDetails> CartDetails { get; set; }
+    }
+}
--- Inveon.Web/Areas/Customer/Controllers/HomeController.cs
+++ Inveon.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Inveon.Web.Hubs;
 using Inveon.Web.Models;
+using Inveon.Web.Services;
 using Inveon.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
@@ -15,11 +17,13 @@ namespace Inveon.Web.Areas.Customer.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IProductService _productService;
         private readonly IHubContext<ChatHub> _chatHub;
-        public HomeController(ILogger<HomeController> logger, IProductService productService, IHubContext<ChatHub> chatHub)
+        private readonly ICartService _cartService;
+        public HomeController(ILogger<HomeController> logger, IProductService productService, IHubContext<ChatHub> chatHub,ICartService cartService)
         {
             _chatHub = chatHub;
             _logger = logger;
             _productService = productService;
+            _cartService = cartService;
         }
 
         public async Task<IActionResult> Index()
@@ -32,6 +36,57 @@ namespace Inveon.Web.Areas.Customer.Controllers
             }
             return View(list);
 
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int productId)
+        {
+            ProductDto model = new();
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId, "");
+            if (response != null && response.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Details")]
+        [Authorize]
+        public async Task<IActionResult> DetailsPost(ProductDto productDto)
+        {
+            var UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            CartHeaderDto cartHeaderDto = new CartHeaderDto();
+            cartHeaderDto.UserId = UserId;
+            CartDto cartDto = new CartDto();
+            cartDto.CartHeader = cartHeaderDto;
+
+
+            CartDetailsDto cartDetails = new CartDetailsDto()
+            {
+                Count = productDto.Count,
+                ProductId = productDto.ProductId
+            };
+
+            var resp = await _productService.GetProductByIdAsync<ResponseDto>(productDto.ProductId, "");
+            if (resp != null && resp.IsSuccess)
+            {
+                cartDetails.Product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(resp.Result));
+            }
+            List<CartDetailsDto> cartDetailsDtos = new();
+            cartDetailsDtos.Add(cartDetails);
+            cartDto.CartDetails = cartDetailsDtos;
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var addToCartResp = await _cartService.AddToCartAsync2<ResponseDto>(cartDto, accessToken);
+
+            if (addToCartResp != null && addToCartResp.IsSuccess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
         }
 
         public IActionResult Privacy()
